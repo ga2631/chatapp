18a5906847a39aa72b017342f70c9df7446290fb
Tan Huynh Nhat <tanhuynh2631@gmail.com>
2020-02-14 15:15:26 +0700

    Add node_env on server

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
