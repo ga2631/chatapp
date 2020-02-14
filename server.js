43089c01b9a503af557dc957013e95b15b388e04
Tan Huynh Nhat <tanhuynh2631@gmail.com>
2020-02-14 17:07:54 +0700

    Complete skeleton app

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-const port = process.env.PORT || 7000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected ...");
+
+    const port = process.env.PORT || 8000;
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
